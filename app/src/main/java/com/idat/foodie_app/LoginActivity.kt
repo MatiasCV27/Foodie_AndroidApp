fa7f01de35d5a81f18213953d358f5ade5ea9db5
FelixCriollo <felixcriollovigo@gmail.com>
2023-11-15 23:33:24 -0500

    Agregando nuevas imagenes, haciendo responsive el Login/Register y acomodando cosas

--- app/src/main/java/com/idat/foodie_app/LoginActivity.kt
+++ app/src/main/java/com/idat/foodie_app/LoginActivity.kt
@@ -2,10 +2,23 @@ package com.idat.foodie_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnIniciarS = findViewById<Button>(R.id.btnSesion)
+        val btnIniciarG = findViewById<Button>(R.id.btnSGoogle)
+        val txtEmail = findViewById<TextInputEditText>(R.id.txtEmail)
+        val txtpassw = findViewById<TextInputEditText>(R.id.txtPassword)
+
+        //btnIniciarG.setOnClickListener(
+        //    if (txtEmail.getText().isEmpty() && txtpassw.getText().isEmpty()) {
+        //
+        //    }
+        //)
     }
 }
