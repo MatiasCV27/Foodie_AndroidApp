e750af7777ee563d557e9125b2955c18611eb41b
Matias <matiascriollo57@gmail.com>
2023-11-20 14:20:56 -0500

    Hasta aqui se presentara al EC3

--- app/src/main/java/com/idat/foodie_app/UseerAccess/AyudaActivity.kt
+++ app/src/main/java/com/idat/foodie_app/UseerAccess/AyudaActivity.kt
@@ -1,8 +1,13 @@
 package com.idat.foodie_app.UseerAccess
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.idat.foodie_app.MenuPrincipal
 import com.idat.foodie_app.R
 
 class AyudaActivity : AppCompatActivity() {
@@ -10,6 +15,24 @@ class AyudaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ayuda)
         val cerrarFaq: ImageView = findViewById(R.id.cerrarFaq)
+        val btnEnviarAyuda = findViewById<Button>(R.id.btnMandarAyuda)
+        val txtNombre = findViewById<TextInputEditText>(R.id.txtNombreHelp)
+        val txtCorreo = findViewById<TextInputEditText>(R.id.txtCorreoHelp)
+        val txtAsunto = findViewById<TextInputEditText>(R.id.txtAsuntoHelp)
+        val txtDescrip = findViewById<TextInputEditText>(R.id.txtDescipHelp)
+
+        btnEnviarAyuda.setOnClickListener {
+            val nombre = txtNombre.text.toString()
+            val correo = txtCorreo.text.toString()
+            val asunto = txtAsunto.text.toString()
+            val descrip = txtDescrip.text.toString()
+
+            if (nombre.isEmpty() || correo.isEmpty() || asunto.isEmpty() || descrip.isEmpty()) {
+                Toast.makeText(this, "FOODIE: Los campos deben estar llenos para el acceso", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "FOODIE: Esta funcionalidad estar disponible pronto", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         cerrarFaq.setOnClickListener {
             finish()
