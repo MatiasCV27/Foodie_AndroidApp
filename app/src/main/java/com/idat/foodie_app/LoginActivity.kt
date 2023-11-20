5155e3dda9cc4e95fc09c5e750d6a0ee0337d514
FelixCriollo <felixcriollovigo@gmail.com>
2023-11-20 00:12:18 -0500

    Perfeccionando el Login y Register

--- app/src/main/java/com/idat/foodie_app/LoginActivity.kt
+++ app/src/main/java/com/idat/foodie_app/LoginActivity.kt
@@ -13,16 +13,28 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         val btnIniciarS = findViewById<Button>(R.id.btnSesion)
         val btnIniciarG = findViewById<Button>(R.id.btnSGoogle)
+        val btnReistro = findViewById<Button>(R.id.btnRegistrase)
         val txtEmail = findViewById<TextInputEditText>(R.id.txtEmail)
         val txtpassw = findViewById<TextInputEditText>(R.id.txtPassword)
 
         btnIniciarS.setOnClickListener {
+            val email = txtEmail.text.toString()
+            val password = txtpassw.text.toString()
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "FOODIE: Los campos deben estar llenos para el acceso", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
             val intent = Intent(this, MenuPrincipal::class.java)
             startActivity(intent)
         }
 
+        btnReistro.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
         btnIniciarG.setOnClickListener{
-            val mensaje = "FOODIE: Usted a iniciado sesión con Google!"
+            val mensaje = "FOODIE: Esta funcionalidad estara disponible pronto!"
             val duracion = Toast.LENGTH_SHORT
             val toast = Toast.makeText(applicationContext, mensaje, duracion)
             toast.show()
--- app/src/main/java/com/idat/foodie_app/RegisterActivity.kt
+++ app/src/main/java/com/idat/foodie_app/RegisterActivity.kt
@@ -1,11 +1,43 @@
 package com.idat.foodie_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val btnReg = findViewById<Button>(R.id.btnRegister)
+        val btnRegG = findViewById<Button>(R.id.btnRegGoogle)
+        val btnCancelarR = findViewById<Button>(R.id.btnCancelarRegi)
+        val txtEmailR = findViewById<TextInputEditText>(R.id.txtEmailRegis)
+        val txtpasswR = findViewById<TextInputEditText>(R.id.txtPasswordRegis)
+
+        btnReg.setOnClickListener {
+            val email = txtEmailR.text.toString()
+            val password = txtpasswR.text.toString()
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "FOODIE: Los campos deben estar llenos para el registro", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCancelarR.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRegG.setOnClickListener{
+            val mensaje = "FOODIE: Esta funcionalidad estara disponible pronto!"
+            val duracion = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, mensaje, duracion)
+            toast.show()
+        }
     }
 }
