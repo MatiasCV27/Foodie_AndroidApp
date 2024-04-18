5df9af90a0b7f20c5c3d1274fb7716c18c5bfa3d
JhKeAn <kefer.0938@hotmail.com>
2024-04-17 19:09:20 -0500

    camvios

--- app/src/main/java/com/idat/foodie_app/UseerAccess/InfoUserActivity.kt
+++ app/src/main/java/com/idat/foodie_app/UseerAccess/InfoUserActivity.kt
@@ -4,8 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import com.idat.foodie_app.R
 import android.os.Bundle
 import android.widget.ImageView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.idat.foodie_app.Adaptadores.AdapterHistorialItems
+import com.idat.foodie_app.Modelos.HistorialItems
+import com.idat.foodie_app.Utils.SelectedUserStats
+import com.idat.foodie_app.databinding.ActivityHistorialBinding
 
 class InfoUserActivity : AppCompatActivity() {
+    
+    private var obtenerGmail = SelectedUserStats.emailUser
+    val db = FirebaseFirestore.getInstance()
+
+    private lateinit var adapterHist: AdapterHistorialItems
+    private lateinit var historiaList: ArrayList<HistorialItems>
+    private lateinit var binding: ActivityHistorialBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info_user)
@@ -16,5 +28,26 @@ class InfoUserActivity : AppCompatActivity() {
             finish()
         }
 
+        obtenerDatos()
+
     }
+
+    private fun obtenerDatos(){
+        historiaList = ArrayList()
+        adapterHist = AdapterHistorialItems(historiaList)
+        db.collection("users").get()
+            .addOnSuccessListener { document ->
+
+                //val wallItem = document.toObject(HistorialItems::class.java)
+                //if (document["email"] == obtenerGmail) {
+                //    wallItem.nombre = document["nombre"].toString()
+                //    wallItem.precios = document["precios"].toString()
+                //    wallItem.fecha = document["fecha"].toString()
+                //    wallItem.precioTotal = document["pTotal"].toString()
+                //    historiaList.add(wallItem)
+
+
+        }
+    }
+
 }
